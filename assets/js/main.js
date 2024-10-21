3beb327cf9629b849226ca139bdc3ae056ba4a64
kau-nakrani <knakrani100@gmail.com>
2024-10-21 18:29:13 +0530

    add new code

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,11 @@ $(document).ready(function(){
         autoplaySpeed: 200000,
         arrows: true,
         dots: true,
+        prevArrow:"<button class='a-left control-c prev slick-prev'><img src='assets/images/icons/arrow-left.svg'></button>",
+        nextArrow:"<button class='a-right control-c next slick-next'><img src='assets/images/icons/arrow-right.svg'></button>"
     });
 });
+
 
 
 const accordionHeaders = document.querySelectorAll('.accordion-header');
