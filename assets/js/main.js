fdae7fe9665ec2f2675c8205febf5d4fa0866adb
kau-nakrani <knakrani100@gmail.com>
2024-10-22 17:46:16 +0530

    add new files

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,11 @@ $(document).ready(function(){
         autoplaySpeed: 200000,
         arrows: true,
         dots: true,
-        prevArrow:"<button class='a-left control-c prev slick-prev'><img src='assets/images/icons/arrow-left.svg'></button>",
-        nextArrow:"<button class='a-right control-c next slick-next'><img src='assets/images/icons/arrow-right.svg'></button>"
+        customPaging: function(slider, i) {
+            return $('<button type="button" data-role="none" role="button" tabindex="0" aria-disabled="false" />').text(i + 1);
+        },
+        prevArrow:"<button class='a-left control-c prev slick-prev' aria-disabled='true'><img src='assets/images/icons/arrow-left.svg' alt='arrow-left'></button>",
+        nextArrow:"<button class='a-right control-c next slick-next' aria-disabled='false'><img src='assets/images/icons/arrow-right.svg' alt='arrow-right'></button>"
     });
 });
 
